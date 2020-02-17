c576e65504f3e6af10bb94717aedcef619f1b5f4
Danielo Rodriguez <rdanielo@gmail.com>
2020-02-17 11:25:54 +0100

    first commit

---
+++ extension.js
@@ -0,0 +1,40 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "copy-by-regex" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerTextEditorCommand('extension.regexCopy', async function (textEditor) {
+		const selection = textEditor.selection;
+		console.log(selection)
+		const regexStr = await vscode.window.showInputBox({
+			placeHolder: 'Input the regex to use'
+		})
+		const text = textEditor.document.getText(new vscode.Range(selection.start,selection.end ))
+		console.log('Text', text)
+	});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
