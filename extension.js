3c17f297bb9cc6dc42c99fbb38010c79fe5e3a04
Danielo Rodriguez <rdanielo@gmail.com>
2020-02-18 07:27:43 +0100

    feat: actual substitution

--- extension.js
+++ extension.js
@@ -1,10 +1,19 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const zip = require('ramda/src/zip');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
+function fullLineRange(line) {
+	new vscode.Range(
+		new vscode.Position(line, 0),
+		new vscode.Position(line, 99),
+
+	)
+}
+let latestRegex = '\\w+';
+let latestInput = '';
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -19,12 +28,29 @@ function activate(context) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerTextEditorCommand('extension.regexCopy', async function (textEditor) {
 		const selection = textEditor.selection;
-		console.log(selection)
-		const regexStr = await vscode.window.showInputBox({
-			placeHolder: 'Input the regex to use'
-		})
-		const text = textEditor.document.getText(new vscode.Range(selection.start,selection.end ))
-		console.log('Text', text)
+		const regexStr = await vscode.window.showInputBox({ placeHolder: 'Input the regex or enter for ' + latestRegex }) || latestRegex
+		latestRegex = regexStr
+		console.log({ regexStr })
+		const text = textEditor.document.getText(new vscode.Range(selection.start, selection.end))
+		const matches = text.match(new RegExp(regexStr, 'g')) || [];
+		latestInput = matches.toString();
+		console.log({ matches })
+		vscode.window.showInformationMessage('Copied the following => ' + matches.join(' | '))
+	});
+	
+	const substituteMatches = (text, [match, substitution]) => text.replace(match, substitution);
+
+	vscode.commands.registerTextEditorCommand('extension.regexPaste', async function (textEditor, edit) {
+		const selection = textEditor.selection;
+		const regexStr = await vscode.window.showInputBox({ placeHolder: 'Input the regex or enter for: ' + latestRegex }) || latestRegex
+		latestRegex = regexStr
+		const inputStr = await vscode.window.showInputBox({ placeHolder: 'Input substitute, leave empty for: ' + latestInput }) || latestInput
+		const text = textEditor.document.getText(new vscode.Range(selection.start, selection.end))
+		const matches = text.match(new RegExp(regexStr, 'g')) || [];
+		console.log({ matches })
+		const allValues = zip(matches, inputStr.split(','))
+		const newText = allValues.reduce(substituteMatches, text)
+		textEditor.edit(eb => eb.replace(selection, newText))
 	});
 
 	context.subscriptions.push(disposable);
@@ -32,7 +58,7 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
